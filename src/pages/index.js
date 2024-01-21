b0feb886681f4d70ce2da2888fe4d15918fed07f
HarshKumarDev <harshkumar6277@gmail.com>
2024-01-21 20:23:50 +0530

    Pilot

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,137 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { useState } from 'react'
 
-const inter = Inter({ subsets: ["latin"] });
+export default function Home () {
+  const [gender, setGender] = useState('')
+  const [startingLetter, setStartingLetter] = useState('')
+  const [regionName, setRegionName] = useState('')
+  const [apiKey, setApiKey] = useState('')
+  const [generatedNames, setGeneratedNames] = useState(null) // Add state for storing generated names
+  const [isLoading, setIsLoading] = useState(false) // Add state for loading state
 
-export default function Home() {
+  const isFormValid = gender && startingLetter && regionName
+
+  const generateName = async () => {
+    if (!isFormValid) {
+      return
+    }
+    setIsLoading(true) // Set loading state to true
+    const payload = {
+      firstLetter: startingLetter,
+      gender: gender,
+      region: regionName,
+      apiKey: apiKey
+    }
+    try {
+      const response = await fetch('/api/nameGenerator', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+      })
+      if (response.ok) {
+        const names = await response.json()
+        setGeneratedNames(JSON.parse(names))
+      } else {
+        throw new Error('Failed to generate names')
+      }
+    } catch (error) {
+      console.error('Name generation error:', error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className='flex min-h-screen flex-col items-center justify-center p-24 bg-gray-100'>
+      <div className='flex flex-col lg:flex-row w-full max-w-6xl mx-auto bg-white rounded-xl shadow-md overflow-hidden'>
+        <div className='lg:w-1/2 p-8'>
+          <h1 className='block mt-1 text-4xl leading-tight font-medium text-black'>
+            Child name generator
+          </h1>
+          <p className='mt-2 text-gray-500'>
+            Generate a unique name for your child
+          </p>
+          <div className='mt-6'>
+            <label className='block text-sm font-medium text-gray-700'>
+              Gender:
+            </label>
+            <select
+              id='gender'
+              className='mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+              value={gender}
+              onChange={e => setGender(e.target.value)}
+            >
+              <option value=''>Select Gender</option>
+              <option value='male'>Male</option>
+              <option value='female'>Female</option>
+            </select>
+
+            <label className='block mt-4 text-sm font-medium text-gray-700'>
+              Starting Letter:
+            </label>
+            <input
+              type='text'
+              id='startingLetter'
+              maxLength='1'
+              className='mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+              value={startingLetter}
+              onChange={e => setStartingLetter(e.target.value)}
             />
-          </a>
+            <label className='block mt-4 text-sm font-medium text-gray-700'>
+              Region:
+            </label>
+            <select
+              id='regionName'
+              className='mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+              value={regionName}
+              onChange={e => setRegionName(e.target.value)}
+            >
+              <option value=''>Select Region</option>
+              <option value='random'>Random</option>
+              <option value='indian'>Indian</option>
+              <option value='american'>American</option>
+              <option value='russian'>Russian</option>
+              <option value='german'>German</option>
+            </select>
+            <label className='block mt-12 text-sm font-medium text-gray-700'>
+              OpenAI API Key:
+            </label>
+            <input
+              type='text'
+              id='apiKey'
+              className='mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+              value={apiKey}
+              onChange={e => setApiKey(e.target.value)}
+            />
+            <button
+              onClick={generateName}
+              className={`mt-6 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
+                isFormValid ? '' : 'opacity-50 cursor-not-allowed'
+              }`}
+              disabled={!isFormValid}
+            >
+              {isLoading ? (
+                <span className='animate-pulse'>Generating...</span>
+              ) : (
+                'Generate'
+              )}
+            </button>
+          </div>
+        </div>
+        <div className='lg:w-1/2 p-8 bg-gray-50 text-center'>
+          {generatedNames && (
+            <div className='h-full flex flex-col justify-center'>
+              <h2 className='text-lg text-gray-700'>AI suggested names:</h2>
+              <ul className='mt-2 text-gray-500 text-xl font-medium list-disc pl-5'>
+                {Object.entries(generatedNames).map(([key, value]) => (
+                  <p key={key}>{value}</p>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  );
+  )
 }
